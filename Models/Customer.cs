2dc68fa845ff8007a5ccc88ad54d1b4eb6bcb2d4
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-11-25 18:00:16 +0200

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadar_Raul_Lab2.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string BirthDate { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadar_Raul_Lab2.Models
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
