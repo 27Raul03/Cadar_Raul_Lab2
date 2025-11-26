f74e8f43eb14c1c2a61105e1b874bc1e56bc4917
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-11-26 18:08:51 +0200

    Ex 1-4

--- Data/LibraryContext.cs
+++ Data/LibraryContext.cs
@@ -18,6 +18,7 @@ namespace Cadar_Raul_Lab2.Data
         public DbSet<Cadar_Raul_Lab2.Models.Customer> Customer { get; set; } = default!;
         public DbSet<Cadar_Raul_Lab2.Models.Genre> Genre { get; set; } = default!;
         public DbSet<Cadar_Raul_Lab2.Models.Author> Author { get; set; } = default!;
+        public DbSet<Cadar_Raul_Lab2.Models.Order> Order { get; set; } = default!;
     }
 }
 
